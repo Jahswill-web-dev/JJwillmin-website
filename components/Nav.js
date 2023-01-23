029f8c4c9c863971d360dd3e2500d595b0596477
Jahswill <onuegbujahswill@gmail.com>
2023-01-23 11:53:06 +0100

    mobile nav and hero completed

--- components/Nav.js
+++ components/Nav.js
@@ -1,53 +1,62 @@
 import Link from 'next/link'
+import { React, useEffect, useState } from 'react';
 
 export default function Nav() {
+
+    const [nav, setNav] = useState(false);
+    const [color, setColor] = useState('transparent');
+    const [textColor, setTextColor] = useState('white');
+
+    const handleNav = () => {
+        setNav(!nav);
+    }
+    useEffect(() => {
+        const changeColor = () => {
+            if (window.scrollY >= 90) {
+                setColor('#ffff');
+            }else{
+                setColor('transparent')
+            }
+        }
+        window.addEventListener('scroll', changeColor)
+    }, []);
+
+
     return (
-        <nav className="navbar bg-white text-xl max-w-5xl max-w-[92%] md:max-w-[910px] mx-auto
-        ">
+        <>
+            {/* mobile nav */}
+            <nav style={{ backgroundColor: `${color}` }} className="navbar bg-white text-xl mx-auto w-full fixed sm:hidden z-10">
+                <div className="flex justify-between max-w-[90%] mx-auto p-2">
+                    <div className="logo text-secondary font-bold mt-1">
+                        <a href="#home">JJWILLMIN</a>
+                    </div>
+                    <div onClick={handleNav} className="hamburger text-3xl sm:hidden ">
+                        <i class="fa-solid fa-bars"></i>
+                    </div>
+                </div>
+                <div className={
+                    nav ?
+                        `absolute top-0 bottom-0 left-0 right-0 bg-black w-full h-screen text-center
+                     ease-in-out duration-300 flex justify-center items-center text-white`
+                        :
+                        `absolute top-0 bottom-0 left-[-600px] right-0 bg-black/70 w-full h-screen text-center
+                     ease-in-out duration-300 flex justify-center items-center text-white`} >
 
-            <div className="flex justify-between  p-4">
-
-                <div className="logo text-secondary font-bold mt-1">
-
-                    <a href="#home">JJWILLMIN</a>
+                    <div onClick={handleNav} className="absolute top-8 right-10 text-4xl ">
+                        <i className="fa-solid fa-x"></i>
+                    </div>
+                    <ul className="">
+                        <li className="nav-item py-2 px-4 text-3xl hover:text-gray-500"><Link href="/">ABOUT</Link></li>
+                        <li className="nav-item py-2 px-4 text-3xl hover:text-gray-500"><Link href="/">SERVICES</Link></li>
+                        <li className="nav-item py-2 px-4 text-3xl hover:text-gray-500"><Link href="/">PROJECTS</Link></li>
+                        <li className="nav-item py-2 px-4 text-3xl hover:text-gray-500"><Link href="/">CONTACT US</Link></li>
+                    </ul>
 
                 </div>
+            </nav>
+            {/* desktop Nav */}
 
-                <div className="hamburger text-3xl sm:hidden"><i class="fa-solid fa-bars"></i></div>
-
-                <ul className="nav-list absolute static mt-11 bg-green w-full left-0 text-[1rem] h-0 overflow hidden
-                sm:relative sm:mt-0 sm:w-auto sm:flex
-                ">
-
-                    <a href="#about" className="nav-link">
-
-                        <li className="nav-item py-2 px-4">ABOUT</li>
-
-                    </a>
-
-                    <Link href="#services" className="nav-link">
-
-                        <li className="nav-item py-2 px-4">SERVICES</li>
-
-                    </Link>
-
-                    <Link href="#projects" className="nav-link">
-
-                        <li className="nav-item py-2 px-4">PROJECTS</li>
-
-                    </Link>
-
-                    <Link href="#contact" className="nav-link">
-
-                        <li className="nav-item py-2 px-4">CONTACT US</li>
-
-                    </Link>
-
-                </ul>
-
-            </div>
-
-        </nav>
+        </>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Header from '../components/Head'
+import Header from '../components/Header'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
@@ -7,15 +7,33 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="max-w-[90%] md:max-w-[1234px] mx-auto bg-black">
+    <>
+    
+    <div className="flex justify-center items-center bg-cover bg-center bg-hero bg-fixed h-screen ">
      
      <Header 
      title="J.J.WILLMIN | Construction Company"
      />
 
-    
-
-
+      {/* overlay */}
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/30 z-[1]"/>
+      {/* hero contents */}
+    <header className="max-w-[90%] md:max-w-[1234px] mx-auto z-[2]">
+      <h1 className="uppercase text-center text-white text-5xl">J.J.WILLMIN</h1>
+      <p className="uppercase  text-center text-white">Civil Engineering works and Procrument Services</p>
+    </header>
     </div>
+    {/* About Section */}
+    <div className="flex">
+      {/* text */}
+      <div>
+
+      </div>
+      {/* Image */}
+      <div>
+
+      </div>
+    </div>
+      </>
   )
 }
